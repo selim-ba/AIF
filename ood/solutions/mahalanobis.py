19e91462def41c6a04e59ca2a46e44c81e081b96
yoyod <79905345+yoyod@users.noreply.github.com>
2025-10-20 12:50:42 +0200

    update ood lab project

--- ood/solutions/mahalanobis.py
+++ ood/solutions/mahalanobis.py
@@ -9,20 +9,24 @@ class Mahalanobis():
         self.mus = {}
         covs = {}
         for label in self.labels:
-            label_features = features[labels == label]
-            self.mus[label] = np.mean(label_features, axis=0)
-            covs[label] = np.cov(label_features.T)
+            label_mask  = (labels == label)
+            label_features = features[label_mask]
+            self.mus[label.item()] = label_features.mean(dim=0)
+            covs[label.item()] = torch.cov(label_features.T) * label_features.size(0)
 
-        cov = np.sum(list(covs.values()), axis=0) / len(features)
-        self.inv_cov = np.linalg.pinv(cov)
+        cov = sum(covs.values()) / features.size(0)
+        self.inv_cov = torch.linalg.pinv(cov)
 
     def _mahalanobis_distance(self, x, mu, inv_cov):
         diff = x - mu
-        return np.dot(np.dot(diff, inv_cov), diff)
+        return diff @ inv_cov @ diff.T
 
     def compute_scores(self, test_features):
         scores = []
         for test_feature in test_features:
-            distances = [self._mahalanobis_distance(test_feature, self.mus[label], self.inv_cov) for label in self.labels]
-            scores.append(- np.min(distances))
-        return scores
+            distances = torch.tensor([
+                self._mahalanobis_distance(test_feature, self.mus[label.item()], self.inv_cov)
+                for label in self.labels
+            ])
+            scores.append(-torch.min(distances))
+        return torch.stack(scores).cpu().numpy()
